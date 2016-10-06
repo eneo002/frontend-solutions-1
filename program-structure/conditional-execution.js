b0ae24a1a0351cd80d122d4b1390bc1c5fdeb8d8
eneo002 <eneo002@e.ntu.edu.sg>
2016-10-06 20:40:58 +0800

    Update conditional-execution.js

--- program-structure/conditional-execution.js
+++ program-structure/conditional-execution.js
@@ -3,6 +3,9 @@
  * Write a function that greets the user by name, or by saying "Hello, World!" if no name is given.
  */
 
-exports.hello = function (name) {
-  your_answer
+exports.hello = function hello (name) {
+  if (!name) { // !name means if there is no name, carry out this statement    // only change the text in the double quotes
+    return "Hello, World!"
+  } else {
+    return "Hello, (name)!"
 }
